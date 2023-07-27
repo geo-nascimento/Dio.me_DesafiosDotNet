9377cfb09863e12f2d116d476978774acfc8873e
geo-nascimento <geo.nascimento.jog@gmail.com>
2023-07-27 19:09:57 -0300

    Exercicio API

---
+++ 05_API/GerenciadorsDeTarefas/Models/Tarefa.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorsDeTarefas.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = null!;
+        public string Descricao { get; set; } = null!;
+        public DateTime Data { get; set; }
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ 05_API/GerenciadorsDeTarefas/Repository/ITarefasRepository.cs
@@ -0,0 +1,14 @@
+﻿using GerenciadorsDeTarefas.Models;
+
+
+namespace GerenciadorsDeTarefas.Repository
+{
+    public interface ITarefasRepository
+    {
+        void CriarTarefa(Tarefa tarefa);
+        Tarefa ObterTarefa(int id);
+        void DeletarTarefa(int id);
+        void AtualizarTarefa(Tarefa tarefa);
+        List<Tarefa> ObterTodas();
+    }
+}
